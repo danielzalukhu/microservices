44280807139b321025580f710f5f90d71a12fa58
Daniel Zalukhu <dnielzlukhu@gmail.com>
2021-11-14 17:59:44 +0700

    update integration apigateway with service-course

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
 const mentorHandler = require("./handler/courses/mentor");
 const courseHandler = require("./handler/courses/course");
 
 /* Mentor route listing. */
-router.get("/mentor/", mentorHandler.read);
-router.post("/mentor/", mentorHandler.create);
-router.get("/mentor/:id", mentorHandler.show);
-router.put("/mentor/:id", mentorHandler.update);
-router.delete("/mentor/:id", mentorHandler.destroy);
+router.get("/mentor/", verifyToken, mentorHandler.read);
+router.post("/mentor/", verifyToken, mentorHandler.create);
+router.get("/mentor/:id", verifyToken, mentorHandler.show);
+router.put("/mentor/:id", verifyToken, mentorHandler.update);
+router.delete("/mentor/:id", verifyToken, mentorHandler.destroy);
 
 /* Course route listing. */
 router.get("/catalog", courseHandler.get);
-router.post("/catalog", courseHandler.create);
+router.post("/catalog", verifyToken, courseHandler.create);
 router.get("/catalog/:id", courseHandler.show);
+router.put("/catalog/:id", verifyToken, courseHandler.update);
+router.delete("/catalog/:id", verifyToken, courseHandler.destroy);
 
 module.exports = router;
--- api-gateway/routes/handler/courses/course/get.js
+++ api-gateway/routes/handler/courses/course/get.js
@@ -1,5 +1,5 @@
 const apiAdapter = require("../../../apiAdapter");
-const { URL_SERVICE_COURSE } = process.env;
+const { URL_SERVICE_COURSE, URL_API_GATEWAY } = process.env;
 
 const api = apiAdapter(URL_SERVICE_COURSE);
 
@@ -7,8 +7,35 @@ module.exports = async(req, res) => {
     try {
         // panggil api (endpoin) dari service course yang dibutuhkan
         // kalau handler get panggil route get dsb
-        const course = await api.get("api/course");
-        return res.json(course.data);
+        const course = await api.get("api/course", {
+            /* we can custom params as an attribute to show only selected fields */
+            params: {
+                ...req.query,
+                // status: "draft",
+            },
+        });
+
+        const coursesData = course.data;
+        const firstPage = coursesData.data.first_page_url.split("?").pop();
+        const lastPage = coursesData.data.last_page_url.split("?").pop();
+
+        coursesData.data.first_page_url = `${URL_API_GATEWAY}/courses/catalog?${firstPage}`;
+        coursesData.data.last_page_url = `${URL_API_GATEWAY}/courses/catalog?${lastPage}`;
+
+        if (coursesData.data.next_page_url) {
+            const nextPage = coursesData.data.next_page_url.split("?").pop();
+            coursesData.data.next_page_url = `${URL_API_GATEWAY}/courses/catalog?${nextPage}`;
+        }
+
+        if (coursesData.data.prev_page_url) {
+            const prevPage = coursesData.data.prev_page_url.split("?").pop();
+            coursesData.data.prev_page_url = `${URL_API_GATEWAY}/courses/catalog?${prevPage}`;
+        }
+
+        coursesData.data.path = `${URL_API_GATEWAY}/courses/catalog`;
+        // console.log(coursesData.data.path);
+
+        return res.json(coursesData);
     } catch (error) {
         if (error.code === "ECONNREFUSED") {
             return res.status(500).json({ status: "error", message: "service unavailable" });
